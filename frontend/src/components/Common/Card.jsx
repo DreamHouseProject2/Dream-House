3ffd656556d00fe1719371bae07772a7bb92879b
aarrjjww <alexiswashington2977@yahoo.com>
2022-04-18 19:12:25 -0700

    completed the components of  card category and find

---
+++ frontend/src/components/Common/Card.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {useDispatch,useSelector} from 'react-redux'
+
+const Card =({home}) =>{
+    const dispatch = useDispatch();
+    const selector = useSelector(state=>state);
+    return(
+        <li key={home.id} class='box'>
+            <img src={'https://res.cloudinary.com/dwzjr9dg5/'+home.main_image} alt="" />
+            <h3>{home.price}</h3>
+            <p>
+                {home.layout} 1,800 sqft <br />
+                {home.state},{home.address}
+            </p>
+        </li>
+    )
+}
+export default Card;
