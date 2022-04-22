db66606e6b8cb24fe61eb7e671bab381d31f94c7
aarrjjww <alexiswashington2977@yahoo.com>
2022-04-21 19:21:37 -0700

    final push of the code

---
+++ frontend/src/components/Common/SaleImage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ImageBackgroundSale from '../../assets/img/background-sell.png'
+function SaleImage() {
+  return (
+    <div>
+    <img src={ImageBackgroundSale} alt="" className="background" />
+    </div>
+  )
+}
+
+export default SaleImage
