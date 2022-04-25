98f6edad30f7d702b2269c4e51dc095ed3b14220
DreamHouseProject2 <103609013+DreamHouseProject2@users.noreply.github.com>
2022-04-26 03:09:12 +0530

    Update API.js

--- frontend/src/API.js
+++ frontend/src/API.js
@@ -7,7 +7,7 @@ var baseURL;
 // } else {
 //     baseURL = 'http://127.0.0.1:8000';
 // }
-baseURL = 'https://backend-yandys.herokuapp.com/';
+baseURL = 'https://backend-dream-house.herokuapp.com/';
 
 const api = axios.create({
     baseURL: baseURL,
